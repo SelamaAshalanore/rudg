d6ee8243e107c7573917ea73ff750ae11049cca5
zhai yao <zhaiyaopku@gmail.com>
2022-04-23 15:07:55 +0800

    parse one function

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
+use ra_ap_syntax::{SourceFile, Parse, ast::{self, HasModuleItem, HasName}};
+
 pub fn parse_code(code: &str) -> Vec<String> {
-    return vec![String::from("test")]
+    let parse: Parse<SourceFile> = SourceFile::parse(code);
+    let file: SourceFile = parse.tree();
+
+    let mut func = None;
+    for item in file.items() {
+        match item {
+            ast::Item::Fn(f) => func = Some(f),
+            _ => unreachable!(),
+        }
+    }
+    let func_name: String = func.unwrap().name().unwrap().text().to_string();
+
+    return vec![func_name]
 }
