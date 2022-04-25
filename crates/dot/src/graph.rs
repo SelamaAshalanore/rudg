982144ae440ff4fa99f3feec570a974cfc64e66b
zhai yao <zhaiyaopku@gmail.com>
2022-04-25 15:11:19 +0800

    extract graph kind

--- crates/dot/src/graph.rs
+++ crates/dot/src/graph.rs
@@ -123,3 +123,31 @@ impl<'a> GraphWalk<'a, Node, &'a Edge> for LabelledGraphWithEscStrs {
         edge.to
     }
 }
+
+
+/// Graph kind determines if `digraph` or `graph` is used as keyword
+/// for the graph.
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
+pub enum Kind {
+    Digraph,
+    Graph,
+}
+
+impl Kind {
+    /// The keyword to use to introduce the graph.
+    /// Determines which edge syntax must be used, and default style.
+    pub fn keyword(&self) -> &'static str {
+        match *self {
+            Kind::Digraph => "digraph",
+            Kind::Graph => "graph"
+        }
+    }
+
+    /// The edgeop syntax to use for this graph kind.
+    pub fn edgeop(&self) -> &'static str {
+        match *self {
+            Kind::Digraph => "->",
+            Kind::Graph => "--",
+        }
+    }
+}
--- crates/dot/src/lib.rs
+++ crates/dot/src/lib.rs
@@ -275,10 +275,9 @@ pub use style::Style;
 pub use arrow::{Arrow, ArrowShape, Side};
 pub use node::{Node, NodeLabels, Trivial};
 pub use edge::{edge, edge_with_arrows, Edge};
-pub use graph::{GraphWalk, LabelledGraph, LabelledGraphWithEscStrs, Nodes, Edges};
+pub use graph::{GraphWalk, LabelledGraph, LabelledGraphWithEscStrs, Nodes, Edges, Kind};
 pub use id::{Id};
 
-use std::borrow::Cow;
 use std::io::prelude::*;
 use std::io;
 
@@ -408,32 +407,6 @@ pub trait Labeller<'a,N,E> {
 
 
 
-/// Graph kind determines if `digraph` or `graph` is used as keyword
-/// for the graph.
-#[derive(Copy, Clone, PartialEq, Eq, Debug)]
-pub enum Kind {
-    Digraph,
-    Graph,
-}
-
-impl Kind {
-    /// The keyword to use to introduce the graph.
-    /// Determines which edge syntax must be used, and default style.
-    fn keyword(&self) -> &'static str {
-        match *self {
-            Kind::Digraph => "digraph",
-            Kind::Graph => "graph"
-        }
-    }
-
-    /// The edgeop syntax to use for this graph kind.
-    fn edgeop(&self) -> &'static str {
-        match *self {
-            Kind::Digraph => "->",
-            Kind::Graph => "--",
-        }
-    }
-}
 
 
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
