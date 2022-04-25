e0b6b1e85b47b87eaa3bb72d9aa756b820a33403
zhai yao <zhaiyaopku@gmail.com>
2022-04-25 15:20:05 +0800

    extract labeller

--- crates/dot/src/lib.rs
+++ crates/dot/src/lib.rs
@@ -269,6 +269,7 @@ pub mod node;
 pub mod edge;
 pub mod graph;
 pub mod id;
+pub mod labeller;
 
 pub use label_text::LabelText::{self, LabelStr, EscStr, HtmlStr};
 pub use style::Style;
@@ -277,6 +278,7 @@ pub use node::{Node, NodeLabels, Trivial};
 pub use edge::{edge, edge_with_arrows, Edge};
 pub use graph::{GraphWalk, LabelledGraph, LabelledGraphWithEscStrs, Nodes, Edges, Kind};
 pub use id::{Id, id_name};
+pub use labeller::{Labeller};
 
 use std::io::prelude::*;
 use std::io;
@@ -324,85 +326,6 @@ use std::io;
 /// elaborate (and non-unique) label text that is used in the graphviz
 /// rendered output.
 
-/// The graph instance is responsible for providing the DOT compatible
-/// identifiers for the nodes and (optionally) rendered labels for the nodes and
-/// edges, as well as an identifier for the graph itself.
-pub trait Labeller<'a,N,E> {
-    /// Must return a DOT compatible identifier naming the graph.
-    fn graph_id(&'a self) -> Id<'a>;
-
-    /// Maps `n` to a unique identifier with respect to `self`. The
-    /// implementer is responsible for ensuring that the returned name
-    /// is a valid DOT identifier.
-    fn node_id(&'a self, n: &N) -> Id<'a>;
-
-    /// Maps `n` to one of the [graphviz `shape` names][1]. If `None`
-    /// is returned, no `shape` attribute is specified.
-    ///
-    /// [1]: http://www.graphviz.org/content/node-shapes
-    fn node_shape(&'a self, _node: &N) -> Option<LabelText<'a>> {
-        None
-    }
-
-    /// Maps `n` to a label that will be used in the rendered output.
-    /// The label need not be unique, and may be the empty string; the
-    /// default is just the output from `node_id`.
-    fn node_label(&'a self, n: &N) -> LabelText<'a> {
-        LabelStr(self.node_id(n).name())
-    }
-
-    /// Maps `e` to a label that will be used in the rendered output.
-    /// The label need not be unique, and may be the empty string; the
-    /// default is in fact the empty string.
-    fn edge_label(&'a self, e: &E) -> LabelText<'a> {
-        let _ignored = e;
-        LabelStr("".into())
-    }
-
-    /// Maps `n` to a style that will be used in the rendered output.
-    fn node_style(&'a self, _n: &N) -> Style {
-        Style::None
-    }
-
-    /// Maps `n` to one of the [graphviz `color` names][1]. If `None`
-    /// is returned, no `color` attribute is specified.
-    ///
-    /// [1]: https://graphviz.gitlab.io/_pages/doc/info/colors.html
-    fn node_color(&'a self, _node: &N) -> Option<LabelText<'a>> {
-        None
-    }
-
-    /// Maps `e` to arrow style that will be used on the end of an edge.
-    /// Defaults to default arrow style.
-    fn edge_end_arrow(&'a self, _e: &E) -> Arrow {
-        Arrow::default()
-    }
-
-    /// Maps `e` to arrow style that will be used on the end of an edge.
-    /// Defaults to default arrow style.
-    fn edge_start_arrow(&'a self, _e: &E) -> Arrow {
-        Arrow::default()
-    }
-
-    /// Maps `e` to a style that will be used in the rendered output.
-    fn edge_style(&'a self, _e: &E) -> Style {
-        Style::None
-    }
-
-    /// Maps `e` to one of the [graphviz `color` names][1]. If `None`
-    /// is returned, no `color` attribute is specified.
-    ///
-    /// [1]: https://graphviz.gitlab.io/_pages/doc/info/colors.html
-    fn edge_color(&'a self, _e: &E) -> Option<LabelText<'a>> {
-        None
-    }
-
-    /// The kind of graph, defaults to `Kind::Digraph`.
-    #[inline]
-    fn kind(&self) -> Kind {
-        Kind::Digraph
-    }
-}
 
 
 
@@ -575,76 +498,6 @@ pub fn render_opts<'a,
 
 
 
-impl<'a> Labeller<'a, Node, &'a Edge> for LabelledGraph {
-    fn graph_id(&'a self) -> Id<'a> {
-        Id::new(&self.name[..]).unwrap()
-    }
-    fn node_id(&'a self, n: &Node) -> Id<'a> {
-        id_name(n)
-    }
-    fn node_label(&'a self, n: &Node) -> LabelText<'a> {
-        match self.node_labels[*n] {
-            Some(ref l) => LabelStr((*l).into()),
-            None => LabelStr(id_name(n).name()),
-        }
-    }
-    fn edge_label(&'a self, e: &&'a Edge) -> LabelText<'a> {
-        LabelStr(e.label.into())
-    }
-    fn node_style(&'a self, n: &Node) -> Style {
-        self.node_styles[*n]
-    }
-    fn edge_style(&'a self, e: &&'a Edge) -> Style {
-        e.style
-    }
-    fn edge_color(&'a self, e: &&'a Edge) -> Option<LabelText<'a>>
-    {
-        match e.color {
-            Some(l) => {
-                Some(LabelStr((*l).into()))
-            },
-            None => None,
-        }
-    }
-    fn edge_end_arrow(&'a self, e: &&'a Edge) -> Arrow {
-        e.end_arrow.clone()
-    }
-
-    fn edge_start_arrow(&'a self, e: &&'a Edge) -> Arrow {
-        e.start_arrow.clone()
-    }
-}
-
-impl<'a> Labeller<'a, Node, &'a Edge> for LabelledGraphWithEscStrs {
-    fn graph_id(&'a self) -> Id<'a> {
-        self.graph.graph_id()
-    }
-    fn node_id(&'a self, n: &Node) -> Id<'a> {
-        self.graph.node_id(n)
-    }
-    fn node_label(&'a self, n: &Node) -> LabelText<'a> {
-        match self.graph.node_label(n) {
-            LabelStr(s) | EscStr(s) | HtmlStr(s) => EscStr(s),
-        }
-    }
-    fn node_color(&'a self, n: &Node) -> Option<LabelText<'a>> {
-        match self.graph.node_color(n) {
-            Some(LabelStr(s)) | Some(EscStr(s)) | Some(HtmlStr(s)) => Some(EscStr(s)),
-            None => None,
-        }
-    }
-    fn edge_label(&'a self, e: &&'a Edge) -> LabelText<'a> {
-        match self.graph.edge_label(e) {
-            LabelStr(s) | EscStr(s) | HtmlStr(s) => EscStr(s),
-        }
-    }
-    fn edge_color(&'a self, e: &&'a Edge) -> Option<LabelText<'a>> {
-        match self.graph.edge_color(e) {
-            Some(LabelStr(s)) | Some(EscStr(s)) | Some(HtmlStr(s)) => Some(EscStr(s)),
-            None => None,
-        }
-    }
-}
 
 
 pub fn graph_to_string(g: LabelledGraph) -> io::Result<String> {
