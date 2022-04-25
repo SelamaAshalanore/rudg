7a3576cdee651088e2b5c931f0fadf38ad8b0d5d
zhai yao <zhaiyaopku@gmail.com>
2022-04-25 15:02:36 +0800

    extract graph

---
+++ crates/dot/src/graph.rs
@@ -0,0 +1,125 @@
+use crate::{
+    node::{Node, Trivial},
+    edge::{Edge},
+    style::{Style}
+};
+use std::borrow::Cow;
+
+pub type Nodes<'a,N> = Cow<'a,[N]>;
+pub type Edges<'a,E> = Cow<'a,[E]>;
+
+// (The type parameters in GraphWalk should be associated items,
+// when/if Rust supports such.)
+
+/// GraphWalk is an abstraction over a graph = (nodes,edges)
+/// made up of node handles `N` and edge handles `E`, where each `E`
+/// can be mapped to its source and target nodes.
+///
+/// The lifetime parameter `'a` is exposed in this trait (rather than
+/// introduced as a generic parameter on each method declaration) so
+/// that a client impl can choose `N` and `E` that have substructure
+/// that is bound by the self lifetime `'a`.
+///
+/// The `nodes` and `edges` method each return instantiations of
+/// `Cow<[T]>` to leave implementers the freedom to create
+/// entirely new vectors or to pass back slices into internally owned
+/// vectors.
+pub trait GraphWalk<'a, N: Clone, E: Clone> {
+    /// Returns all the nodes in this graph.
+    fn nodes(&'a self) -> Nodes<'a, N>;
+    /// Returns all of the edges in this graph.
+    fn edges(&'a self) -> Edges<'a, E>;
+    /// The source node for `edge`.
+    fn source(&'a self, edge: &E) -> N;
+    /// The target node for `edge`.
+    fn target(&'a self, edge: &E) -> N;
+}
+
+
+
+pub struct LabelledGraph {
+    /// The name for this graph. Used for labelling generated `digraph`.
+    pub name: &'static str,
+
+    /// Each node is an index into `node_labels`; these labels are
+    /// used as the label text for each node. (The node *names*,
+    /// which are unique identifiers, are derived from their index
+    /// in this array.)
+    ///
+    /// If a node maps to None here, then just use its name as its
+    /// text.
+    pub node_labels: Vec<Option<&'static str>>,
+
+    pub node_styles: Vec<Style>,
+
+    /// Each edge relates a from-index to a to-index along with a
+    /// label; `edges` collects them.
+    edges: Vec<Edge>,
+}
+
+// A simple wrapper around LabelledGraph that forces the labels to
+// be emitted as EscStr.
+pub struct LabelledGraphWithEscStrs {
+    pub graph: LabelledGraph,
+}
+
+
+
+impl LabelledGraph {
+    pub fn new(name: &'static str,
+            node_labels: Trivial,
+            edges: Vec<Edge>,
+            node_styles: Option<Vec<Style>>)
+            -> LabelledGraph {
+        let count = node_labels.len();
+        LabelledGraph {
+            name: name,
+            node_labels: node_labels.into_opt_strs(),
+            edges: edges,
+            node_styles: match node_styles {
+                Some(nodes) => nodes,
+                None => vec![Style::None; count],
+            },
+        }
+    }
+}
+
+impl LabelledGraphWithEscStrs {
+    pub fn new(name: &'static str,
+            node_labels: Trivial,
+            edges: Vec<Edge>)
+            -> LabelledGraphWithEscStrs {
+        LabelledGraphWithEscStrs { graph: LabelledGraph::new(name, node_labels, edges, None) }
+    }
+}
+
+
+impl<'a> GraphWalk<'a, Node, &'a Edge> for LabelledGraph {
+    fn nodes(&'a self) -> Nodes<'a, Node> {
+        (0..self.node_labels.len()).collect()
+    }
+    fn edges(&'a self) -> Edges<'a, &'a Edge> {
+        self.edges.iter().collect()
+    }
+    fn source(&'a self, edge: &&'a Edge) -> Node {
+        edge.from
+    }
+    fn target(&'a self, edge: &&'a Edge) -> Node {
+        edge.to
+    }
+}
+
+impl<'a> GraphWalk<'a, Node, &'a Edge> for LabelledGraphWithEscStrs {
+    fn nodes(&'a self) -> Nodes<'a, Node> {
+        self.graph.nodes()
+    }
+    fn edges(&'a self) -> Edges<'a, &'a Edge> {
+        self.graph.edges()
+    }
+    fn source(&'a self, edge: &&'a Edge) -> Node {
+        edge.from
+    }
+    fn target(&'a self, edge: &&'a Edge) -> Node {
+        edge.to
+    }
+}
--- crates/dot/src/lib.rs
+++ crates/dot/src/lib.rs
@@ -267,12 +267,14 @@ pub mod style;
 pub mod arrow;
 pub mod node;
 pub mod edge;
+pub mod graph;
 
 pub use label_text::LabelText::{self, LabelStr, EscStr, HtmlStr};
 pub use style::Style;
 pub use arrow::{Arrow, ArrowShape, Side};
 pub use node::{Node, NodeLabels, Trivial};
 pub use edge::{edge, edge_with_arrows, Edge};
+pub use graph::{GraphWalk, LabelledGraph, LabelledGraphWithEscStrs, Nodes, Edges};
 
 use std::borrow::Cow;
 use std::io::prelude::*;
@@ -456,8 +458,7 @@ pub trait Labeller<'a,N,E> {
 }
 
 
-pub type Nodes<'a,N> = Cow<'a,[N]>;
-pub type Edges<'a,E> = Cow<'a,[E]>;
+
 
 /// Graph kind determines if `digraph` or `graph` is used as keyword
 /// for the graph.
@@ -486,32 +487,6 @@ impl Kind {
     }
 }
 
-// (The type parameters in GraphWalk should be associated items,
-// when/if Rust supports such.)
-
-/// GraphWalk is an abstraction over a graph = (nodes,edges)
-/// made up of node handles `N` and edge handles `E`, where each `E`
-/// can be mapped to its source and target nodes.
-///
-/// The lifetime parameter `'a` is exposed in this trait (rather than
-/// introduced as a generic parameter on each method declaration) so
-/// that a client impl can choose `N` and `E` that have substructure
-/// that is bound by the self lifetime `'a`.
-///
-/// The `nodes` and `edges` method each return instantiations of
-/// `Cow<[T]>` to leave implementers the freedom to create
-/// entirely new vectors or to pass back slices into internally owned
-/// vectors.
-pub trait GraphWalk<'a, N: Clone, E: Clone> {
-    /// Returns all the nodes in this graph.
-    fn nodes(&'a self) -> Nodes<'a, N>;
-    /// Returns all of the edges in this graph.
-    fn edges(&'a self) -> Edges<'a, E>;
-    /// The source node for `edge`.
-    fn source(&'a self, edge: &E) -> N;
-    /// The target node for `edge`.
-    fn target(&'a self, edge: &E) -> N;
-}
 
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
 pub enum RenderOption {
@@ -677,62 +652,6 @@ pub fn render_opts<'a,
 
 
 
-pub struct LabelledGraph {
-    /// The name for this graph. Used for labelling generated `digraph`.
-    name: &'static str,
-
-    /// Each node is an index into `node_labels`; these labels are
-    /// used as the label text for each node. (The node *names*,
-    /// which are unique identifiers, are derived from their index
-    /// in this array.)
-    ///
-    /// If a node maps to None here, then just use its name as its
-    /// text.
-    node_labels: Vec<Option<&'static str>>,
-
-    node_styles: Vec<Style>,
-
-    /// Each edge relates a from-index to a to-index along with a
-    /// label; `edges` collects them.
-    edges: Vec<Edge>,
-}
-
-// A simple wrapper around LabelledGraph that forces the labels to
-// be emitted as EscStr.
-pub struct LabelledGraphWithEscStrs {
-    graph: LabelledGraph,
-}
-
-
-
-impl LabelledGraph {
-    pub fn new(name: &'static str,
-            node_labels: Trivial,
-            edges: Vec<Edge>,
-            node_styles: Option<Vec<Style>>)
-            -> LabelledGraph {
-        let count = node_labels.len();
-        LabelledGraph {
-            name: name,
-            node_labels: node_labels.into_opt_strs(),
-            edges: edges,
-            node_styles: match node_styles {
-                Some(nodes) => nodes,
-                None => vec![Style::None; count],
-            },
-        }
-    }
-}
-
-impl LabelledGraphWithEscStrs {
-    pub fn new(name: &'static str,
-            node_labels: Trivial,
-            edges: Vec<Edge>)
-            -> LabelledGraphWithEscStrs {
-        LabelledGraphWithEscStrs { graph: LabelledGraph::new(name, node_labels, edges, None) }
-    }
-}
-
 fn id_name<'a>(n: &Node) -> Id<'a> {
     Id::new(format!("N{}", *n)).unwrap()
 }
@@ -808,35 +727,6 @@ impl<'a> Labeller<'a, Node, &'a Edge> for LabelledGraphWithEscStrs {
     }
 }
 
-impl<'a> GraphWalk<'a, Node, &'a Edge> for LabelledGraph {
-    fn nodes(&'a self) -> Nodes<'a, Node> {
-        (0..self.node_labels.len()).collect()
-    }
-    fn edges(&'a self) -> Edges<'a, &'a Edge> {
-        self.edges.iter().collect()
-    }
-    fn source(&'a self, edge: &&'a Edge) -> Node {
-        edge.from
-    }
-    fn target(&'a self, edge: &&'a Edge) -> Node {
-        edge.to
-    }
-}
-
-impl<'a> GraphWalk<'a, Node, &'a Edge> for LabelledGraphWithEscStrs {
-    fn nodes(&'a self) -> Nodes<'a, Node> {
-        self.graph.nodes()
-    }
-    fn edges(&'a self) -> Edges<'a, &'a Edge> {
-        self.graph.edges()
-    }
-    fn source(&'a self, edge: &&'a Edge) -> Node {
-        edge.from
-    }
-    fn target(&'a self, edge: &&'a Edge) -> Node {
-        edge.to
-    }
-}
 
 pub fn graph_to_string(g: LabelledGraph) -> io::Result<String> {
     let mut writer = Vec::new();
