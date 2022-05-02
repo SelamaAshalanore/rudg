83c84fedb4a51c824e35100b89910d357fc5104d
zhai yao <zhaiyaopku@gmail.com>
2022-05-02 15:56:01 +0800

    match call expr

--- crates/ast_parser_rust/src/lib.rs
+++ crates/ast_parser_rust/src/lib.rs
@@ -1,4 +1,4 @@
-use ra_ap_syntax::{SourceFile, Parse, ast::{self, HasModuleItem, HasName}};
+use ra_ap_syntax::{SourceFile, Parse, ast::{self, HasModuleItem, HasName}, AstNode};
 use dot::{LabelledGraph, graph_to_string, edge, Edge, Style};
 
 
@@ -14,13 +14,34 @@ pub fn code_to_dot_digraph(code: &str) -> String {
     let mut func_names: Vec<&str> = vec![];
     let mut struct_names: Vec<&str> = vec![];
     let mut impl_names: Vec<(&str, &str)> = vec![];
+    let mut call_dependency_names: Vec<(&str, &str)> = vec![];
 
 
     for item in file.items() {
         // println!("{}", &item);
         match item {
             ast::Item::Fn(f) => {
-                func_names.push(string_to_static_str(f.name().unwrap().text().to_string()));
+                let f_name = string_to_static_str(f.name().unwrap().text().to_string());
+                func_names.push(f_name);
+                for stmt in f.get_or_create_body().statements() {
+                    match stmt {
+                        ast::Stmt::ExprStmt(expr) => {
+                            let exp = expr.expr().unwrap();
+                            match exp {
+                                ast::Expr::CallExpr(call_exp) => {
+                                    let call_expr = call_exp.to_string();
+                                    let call_names: Vec<&str> = call_expr.split("(").collect();
+                                    let call_name = String::from(call_names[0]);
+                                    call_dependency_names.push((string_to_static_str(call_name), f_name));
+                                },
+                                _ => ()
+                            }
+                            
+                        },
+                        _ => ()
+                    };
+                    
+                }
             },
             ast::Item::Impl(ip) => {
                 let struct_name: String = ip.self_ty().unwrap().to_string();
@@ -53,6 +74,14 @@ pub fn code_to_dot_digraph(code: &str) -> String {
         let struct_index = all_names.iter().position(|&name| name == struct_name).unwrap();
         let func_index = all_names.iter().position(|&name| name == func_name).unwrap();
         edge_vec.push(edge(func_index, struct_index, "impl", Style::None, None));
+    }
+
+    for impl_desc in call_dependency_names {
+        let outer_name = impl_desc.0;
+        let inner_name = impl_desc.1;
+        let outer_index = all_names.iter().position(|&name| name == outer_name).unwrap();
+        let inner_index = all_names.iter().position(|&name| name == inner_name).unwrap();
+        edge_vec.push(edge(outer_index, inner_index, "call", Style::None, None));
     }
     
 
