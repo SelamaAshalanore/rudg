c5cabb015d5aed9cc62622c53a2c82adcfe07a77
zhai yao <zhaiyaopku@gmail.com>
2022-05-03 11:43:32 +0800

    use DotEntity to store entity info

--- crates/ast_parser_rust/src/lib.rs
+++ crates/ast_parser_rust/src/lib.rs
@@ -1,7 +1,7 @@
 mod uml_entity;
 
 use ra_ap_syntax::{SourceFile, Parse, ast::{self, HasModuleItem, HasName}};
-use dot::{LabelledGraph, graph_to_string, edge, Edge, Style, id_name};
+use dot::{graph_to_string, edge, Edge, Style, new_graph, Node};
 use uml_entity::DotEntity;
 
 
@@ -14,10 +14,6 @@ pub fn code_to_dot_digraph(code: &str) -> String {
     let parse: Parse<SourceFile> = SourceFile::parse(code);
     let file: SourceFile = parse.tree();
 
-    let mut func_names: Vec<&str> = vec![];
-    let mut struct_names: Vec<&str> = vec![];
-    let mut impl_names: Vec<(&str, &str)> = vec![];
-    let mut call_dependency_names: Vec<(&str, &str)> = vec![];
     let mut dot_entities: Vec<DotEntity> = vec![];
 
 
@@ -26,7 +22,6 @@ pub fn code_to_dot_digraph(code: &str) -> String {
         match item {
             ast::Item::Fn(f) => {
                 let f_name = string_to_static_str(f.name().unwrap().text().to_string());
-                func_names.push(f_name);
                 dot_entities.push(DotEntity::Label(f_name.to_string()));
                 for stmt in f.get_or_create_body().statements() {
                     match stmt {
@@ -37,8 +32,7 @@ pub fn code_to_dot_digraph(code: &str) -> String {
                                     let call_expr = call_exp.to_string();
                                     let call_names: Vec<&str> = call_expr.split("(").collect();
                                     let call_name = String::from(call_names[0]);
-                                    call_dependency_names.push((f_name, (string_to_static_str(call_name.clone()))));
-                                    dot_entities.push(DotEntity::Edge(edge(f_name, call_name.as_str(), "", Style::None, None)))
+                                    dot_entities.push(DotEntity::Edge(edge(f_name, call_name.as_str(), "call", Style::None, None)))
                                 },
                                 _ => ()
                             }
@@ -56,42 +50,18 @@ pub fn code_to_dot_digraph(code: &str) -> String {
                     match impl_func {
                         ast::AssocItem::Fn(f) => {
                             let f_name = f.name().unwrap().text().to_string();
-                            func_names.push(string_to_static_str(f.name().unwrap().text().to_string()));
-                            impl_names.push((string_to_static_str(struct_name.clone()), string_to_static_str(f.name().unwrap().text().to_string())));
                             dot_entities.push(DotEntity::Label(f.name().unwrap().text().to_string()));
-                            dot_entities.push(DotEntity::Edge(edge(struct_name.as_str(), f_name.as_str(), "", Style::None, None)))
+                            dot_entities.push(DotEntity::Edge(edge(f_name.as_str(), struct_name.as_str(), "impl", Style::None, None)))
                         },
                         _ => ()
                     }
                 }
             },
             ast::Item::Struct(st) => {
-                struct_names.push(string_to_static_str(st.name().unwrap().text().to_string()));
                 dot_entities.push(DotEntity::Label(st.name().unwrap().text().to_string()));
             },
             _ => (),
         }
-    }
-
-    let mut all_names: Vec<&str> = struct_names.clone();
-    all_names.append(func_names.as_mut());
-
-
-    let mut edge_vec: Vec<Edge> = vec![];
-    for impl_desc in impl_names {
-        let struct_name = impl_desc.0;
-        let func_name = impl_desc.1;
-        let struct_index = all_names.iter().position(|&name| name == struct_name).unwrap();
-        let func_index = all_names.iter().position(|&name| name == func_name).unwrap();
-        edge_vec.push(edge(id_name(&func_index).as_slice(), id_name(&struct_index).as_slice(), "impl", Style::None, None));
-    }
-
-    for impl_desc in call_dependency_names {
-        let outer_name = impl_desc.0;
-        let inner_name = impl_desc.1;
-        let outer_index = all_names.iter().position(|&name| name == outer_name).unwrap();
-        let inner_index = all_names.iter().position(|&name| name == inner_name).unwrap();
-        edge_vec.push(edge(id_name(&outer_index).as_slice(), id_name(&inner_index).as_slice(), "call", Style::None, None));
     }
 
     let mut label_list: Vec<&str> = vec![];
@@ -106,10 +76,11 @@ pub fn code_to_dot_digraph(code: &str) -> String {
             }
         }
     }
-    
+    let node_list: Vec<Node> = label_list
+                                .iter()
+                                .map(|l| Node::new(l, l, Style::None, None))
+                                .collect();    
+    let new_digraph = new_graph("ast", node_list, edge_list, None);
 
-    let labels = label_list.iter().map(|name| Some(*name)).collect();
-    let digraph = LabelledGraph::new("ast", labels, edge_list, None);
-
-    return graph_to_string(digraph).unwrap();
+    return graph_to_string(new_digraph).unwrap();
 }
