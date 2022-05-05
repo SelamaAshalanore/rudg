c8768b8f3db181f7007da2ac0991408f616b90e0
zhai yao <zhaiyaopku@gmail.com>
2022-05-05 22:17:25 +0800

    remove unused parsing functions

--- crates/ast_parser_rust/src/lib.rs
+++ crates/ast_parser_rust/src/lib.rs
@@ -1,7 +1,7 @@
 mod uml_entity;
 
-use ra_ap_syntax::{SourceFile, Parse, ast::{self, HasModuleItem, HasName, Fn}, AstNode, match_ast};
-use dot::{graph_to_string, edge, Edge, Style, new_graph, Node};
+use ra_ap_syntax::{SourceFile, Parse, ast::{self, HasModuleItem}};
+use dot::{graph_to_string, Edge, Style, new_graph, Node};
 use uml_entity::{DotEntity, UMLFn, UMLClass, UMLModule};
 
 pub fn code_to_dot_digraph(code: &str) -> String {
@@ -10,23 +10,18 @@ pub fn code_to_dot_digraph(code: &str) -> String {
     let parse: Parse<SourceFile> = SourceFile::parse(code);
     let file: SourceFile = parse.tree();
 
-    let mut dot_entities: Vec<DotEntity> = vec![];
-
     // visit all items in SourceFile and extract dot entities from every type of them
     for item in file.items() {
         match item {
             ast::Item::Fn(f) => {
                 let uml_fn = UMLFn::from_ast_fn(&f);
-                dot_entities.append(&mut uml_fn.get_dot_entities());
                 uml_module.add_fn(uml_fn);
             },
             ast::Item::Impl(ip) => {
-                dot_entities.append(&mut get_impl_dot_entities(ip.clone()));
                 uml_module.add_ast_impl(ip);
             },
             ast::Item::Struct(st) => {
                 let uml_class = UMLClass::from_ast_struct(&st);
-                dot_entities.append(&mut uml_class.get_dot_entities());
                 uml_module.add_struct(uml_class);
             },
             _ => (),
@@ -55,47 +50,4 @@ pub fn code_to_dot_digraph(code: &str) -> String {
     let new_digraph = new_graph("ast", node_list, edge_list, None);
 
     return graph_to_string(new_digraph).unwrap();
-}
-
-fn get_fn_dot_entities(f: Fn) -> Vec<DotEntity> {
-    let mut dot_entities = vec![];
-    let f_name = f.name().unwrap().text().to_string();
-    dot_entities.push(DotEntity::Label(f_name.clone()));
-
-    // visit all Fn descendants and process CallExpr
-    for node in f.syntax().descendants() {
-        match_ast! {
-            match node {
-                ast::CallExpr(it) => {
-                    let call_name = get_call_expr_fn_names(it);
-                    dot_entities.push(DotEntity::Edge(edge(&f_name, call_name.as_str(), "call", Style::None, None)))
-                },
-                _ => (),
-            }
-        }
-    }
-    dot_entities
-}
-
-fn get_impl_dot_entities(ip: ast::Impl) -> Vec<DotEntity> {
-    let mut dot_entities = vec![];
-    let struct_name: String = ip.self_ty().unwrap().to_string();
-    let impl_funcs = ip.get_or_create_assoc_item_list().assoc_items();
-    for impl_func in impl_funcs {
-        match impl_func {
-            ast::AssocItem::Fn(f) => {
-                let f_name = f.name().unwrap().text().to_string();
-                dot_entities.append(&mut get_fn_dot_entities(f));
-                dot_entities.push(DotEntity::Edge(edge(f_name.as_str(), struct_name.as_str(), "impl", Style::None, None)))
-            },
-            _ => ()
-        }
-    }
-    dot_entities
-}
-
-fn get_call_expr_fn_names(call_exp: ast::CallExpr) -> String {
-    let call_expr = call_exp.to_string();
-    let call_names: Vec<&str> = call_expr.split("(").collect();
-    String::from(call_names[0])
 }
