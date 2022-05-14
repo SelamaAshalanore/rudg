23ad89a5dedb651cc2d20829f4158c9427376b09
zhai yao <zhaiyaopku@gmail.com>
2022-05-14 19:33:42 +0800

    use clap to parse command line arguments

--- crates/ast_parser_rust/src/main.rs
+++ crates/ast_parser_rust/src/main.rs
@@ -1,41 +1,41 @@
 use staticanalyzer::rs2dot;
 use std::{env, fs};
 use std::path::{Path, PathBuf};
+use clap::{arg, command};
 
-fn main() -> () {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        panic!("Please set a rust file path");
-    }
-    let path = Path::new(&args[1]);
-    let results = rs2dot(path);
-    let mut target_name = PathBuf::new();
+fn main() {
+    let matches = command!()
+        .arg(arg!([file] "Rust source code file path"))
+        .arg(
+            arg!(
+                -o --output <DIR> "Sets a custom output directory"
+            )
+            .required(false)
+            .allow_invalid_utf8(true),
+        )
+        .get_matches();
 
-    match args.len() {
-        2 => {
-            let target_dir = path.parent().unwrap();
-            target_name.push(target_dir.join(path.file_stem().unwrap()));
-            target_name.set_extension(".dot");
-        },
-        4 => {
-            if &args[2] != "-o" {
-                panic!("current options has [-o], please enter the right option");
+    // You can check the value provided by positional arguments, or option arguments
+    if let Some(file) = matches.value_of("file") {
+        let path = Path::new(file);
+        let results = rs2dot(path);
+        let mut target_name = PathBuf::new();
+
+        if let Some(raw_config) = matches.value_of_os("output") {
+            // use output indicated directory path
+            target_name.push(Path::new(raw_config).join(path.file_stem().unwrap()));
+        } else {
+            // use same directory as source file's
+            target_name.push(path.parent().unwrap().join(path.file_stem().unwrap()));
+        }
+
+        target_name.set_extension(".dot");
+        match fs::write(&target_name, results) {
+            Ok(_) => (),
+            Err(e) => {
+                println!("target_name is {:?}", target_name);
+                println!("{:?}", e);
             }
-            let target_dir = Path::new(&args[3]);
-            target_name.push(target_dir.join(path.file_stem().unwrap()));
-            target_name.set_extension(".dot");
-        }
-        _ => {
-            panic!("Please set a rust file path");
-        }
-    }
-
-    
-    match fs::write(&target_name, results) {
-        Ok(_) => (),
-        Err(e) => {
-            println!("target_name is {:?}", target_name);
-            println!("{:?}", e);
         }
     }
 }
