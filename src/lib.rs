401ba7ca14fea110a4075984b66ac3b4b1c9afaf
zhai yao <zhaiyaopku@gmail.com>
2022-05-20 09:52:24 +0800

    check path is file

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ use graph_exporter::{GraphExporter};
 use std::path::Path;
 use std::fs::read_to_string;
 use parser::{ast_parser::AstParser, StringParser};
+use file_reader::get_rs_file_paths;
 
 /// The function `rs2dot` returns graphed file module.
 ///
@@ -20,8 +21,13 @@ use parser::{ast_parser::AstParser, StringParser};
 /// }
 /// ```
 pub fn rs2dot<'a, P: AsRef<Path>>(path: P) -> String {
-    let file_string = read_to_string(path).unwrap();
-    code_to_dot_digraph(&file_string)
+    let p = path.as_ref();
+    if p.is_file() {
+        let file_string = read_to_string(path).unwrap();
+        code_to_dot_digraph(&file_string)
+    } else {
+        String::new()
+    }
 }
 
 pub fn code_to_dot_digraph(code: &str) -> String {
